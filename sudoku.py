1fac1114036d872b8f6076cc9652d81735ec071d
Terry <think@terry>
2013-07-05 19:19:44 -0600

    Refactored a bit.

--- sudoku.py
+++ sudoku.py
@@ -5,22 +5,22 @@ import sys
 
 class Cell():
 
-    INVALID_VALUE = -1  # per http://stackoverflow.com/a/2682752
+    NOT_YET_FILLED = -1  # per http://stackoverflow.com/a/2682752
 
     def __init__(self, value):
         if value != '-':
             self._value = int(value)
         else:
-            self._value = self.INVALID_VALUE
+            self._value = self.NOT_YET_FILLED
 
         self._possibilities = [num for num in range(1, 10)]
 
 
     def __str__(self):
-        if self._value != self.INVALID_VALUE:
-            return str(self.val())
-        else:
+        if self._value == self.NOT_YET_FILLED:
             return ' '
+        else:
+            return str(self.val())
 
 
     def val(self, new_value=None):
@@ -38,21 +38,20 @@ class Cell():
         assert possibility in self.possibilities(), ('Cannot remove %s from %s for cell %s' %
             (possibility, self.possibilities(), self))
 
-        # print 'removing possibility %s...' % possibility
         self._possibilities.remove(possibility)
 
         if len(self.possibilities()) == 0:
-            if self.val() == self.INVALID_VALUE:
+            if self.val() == self.NOT_YET_FILLED:
                 raise Exception('The last possibility %s was just removed from cell %s' % (possibility, self))
 
         if len(self.possibilities()) == 1:
             last_man_standing = self.possibilities()[0]
-            if self.val() == self.INVALID_VALUE:
+            if self.val() == self.NOT_YET_FILLED:
                 print 'Was able to fill in a %s!' % last_man_standing
                 self._possibilities.remove(last_man_standing)
                 self.val(last_man_standing)
             else:
-                pass # it's okay the first time around
+                pass # it's okay the first time around, but TODO raise an exception future times around:
                 # raise Exception('Cannot overwrite cell of value %s with new value %s for cell %s' %
                 #     (self.val(), last_man_standing, self))
 
@@ -151,6 +150,17 @@ class Puzzle():
 
 
     def box_index(self, row_index, col_index):
+        """
+            Calculate which box the intersection of the row and column is in,
+            where the boxes are laid out as:
+
+            0  |  1  |  2
+            ---+-----+----
+            3  |  4  |  5
+            ---+-----+----
+            6  |  7  |  8
+        """
+
         return (row_index // 3) * 3 + (col_index // 3) # integer (floor) division
 
 
@@ -178,54 +188,66 @@ class Puzzle():
     def filled_in(self):
         for i in range(len(self.rows())):
             for j in range(len(self.cols())):
-                if self.puzzle[i][j].val() == Cell.INVALID_VALUE:
+                if self.puzzle[i][j].val() == Cell.NOT_YET_FILLED:
                     return False
         return True
 
 
     def valid_vals(self):
+        """
+            Check that every row, column, and box contains only the numbers 1-9 and no duplicates.
+        """
+
         for row in self.rows():
-            if not self._one_through_nine(row):
+            if not self._one_through_nine_or_empty(row):
                 return False
-                # raise Exception('Solution contains number other than 1-9! Row:\n%s\n' % row)
             dupes = self._num_duplicates(row)
             if dupes > 0:
                 raise Exception('Solution contains %s duplicates in row!\n%s\n!' +
                     'Either the solver has a bug orow the puzzle cannot be solved.' % (dupes, row))
+
         for col in self.cols():
-            if not self._one_through_nine(col):
+            if not self._one_through_nine_or_empty(col):
                 return False
-                # raise Exception('Solution contains number other than 1-9! Column:\n%s\n' % col)
             dupes = self._num_duplicates(col)
             if dupes > 0:
                 raise Exception('Solution contains %s duplicates in column\n%s\n' +
                     'Either the solver has a bug or the puzzle cannot be solved.' % (dupes, col))
+        
         for box in self.boxes():
-            if not self._one_through_nine(box):
+            if not self._one_through_nine_or_empty(box):
                 return False
-                # raise Exception('Solution contains number other than 1-9! Box:\n%s\n' % box)
             dupes = self._num_duplicates(box)
             if dupes > 0:
                 raise Exception('Solution contains %s duplicates in box\n%s\n' +
                     'Either the solver has a bug or the puzzle cannot be solved.' % (dupes, box))
+        
         return True
 
 
     def _num_duplicates(self, array):
-        # per http://stackoverflow.com/a/1541827
-        return len(array) - len(set(array))
+        return len(array) - len(set(array))  # per http://stackoverflow.com/a/1541827
 
 
-    def _one_through_nine(self, array):
-        ideal_array = [i for i in range(1, 10)]
-        array_as_vals = [cell.val() for cell in array]
-        return set(array_as_vals) == set(ideal_array)
+    def _one_through_nine_or_empty(self, array):
+        vals = [cell.val() for cell in array]
+        ideal_vals = [i for i in range(1, 10)]
+
+        # set discards duplicates and ignores ordering
+
+        if set(vals) == set(ideal_vals):
+            return True
+        elif set(vals) == set(ideal_vals + [Cell.NOT_YET_FILLED]):
+            return False
+        else:
+            raise Exception('Row/column/box contains number other than 1-9 and %s: %s'
+                % (Cell.NOT_YET_FILLED, vals))
 
 
 class Solver():
 
     def __init__(self, filename):
-        pass
+        self.filename = filename
 
 
     def run(self):
@@ -248,14 +270,14 @@ class Solver():
 
         if solved:
             print 'Solved puzzle!'
-        else: # not prunable
+        else: # not solved and not prunable
             print 'This puzzle is too difficult!'
 
         self.puzzle.print_puzzle()
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print 'Usage: ' + __file__ + ' [puzzle file path]'
+        print 'Usage: ' + __file__ + ' [puzzle filepath]'
         sys.exit(0)
 
     filename = sys.argv[1]
